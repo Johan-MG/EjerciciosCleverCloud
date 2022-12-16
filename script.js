23ac01d2b3a7d66e9e49c31f8ffe2dcfdc01982b
Johan-MG <johanmg902@gmail.com>
2022-12-15 20:30:10 -0600

    Se agrega la parte visual

--- script.js
+++ script.js
@@ -4,25 +4,30 @@ function digitsSum(inputInt){
     for(let i = 0; i<inputInt.length; i++){
         result += parseInt(inputInt[i]);
     }
+    document.getElementById("resultado1").innerHTML = "Resultado: "+result;
     return result;
 }
 
 function isPalindrome(inputStr){
     for(let i=0; i<inputStr.length; i++){
         if(inputStr[i] != inputStr[inputStr.length - i - 1]){
+            document.getElementById("resultado2").innerHTML = "Resultado: "+false;
             return false;
         }
     }
+    document.getElementById("resultado2").innerHTML = "Resultado: "+true;
     return true;
 }
 
 function maxAdjacentProd(inputArray){
+    inputArray = inputArray.split(',');
     let result = parseInt(inputArray[0]*inputArray[1]);
     for(let i=0;i<inputArray.length-1;i++){
         if(parseInt(inputArray[i]*inputArray[i+1])>result){
             result = parseInt(inputArray[i]*inputArray[i+1]);
         }
     }
+    document.getElementById("resultado3").innerHTML = "Resultado: "+result;
     return result;
 }
 
